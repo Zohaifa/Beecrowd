08a27fe04bc4ad04750307fcc745ee5fda2c27fb
Zohaifa <141559676+Zohaifa@users.noreply.github.com>
2023-11-03 07:01:01 +0600

    Create 1097.c

---
+++ 1097.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int i, j;
+    for(i=1, j=7; i<=9; i+=2, j+=2)    {
+        printf("I=%d J=%d\n", i, j);
+        printf("I=%d J=%d\n", i, j-1);
+        printf("I=%d J=%d\n", i, j-2);
+    }
+
+    return 0;
+}
