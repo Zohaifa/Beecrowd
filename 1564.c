9574387dbbdcca23fe4f5eb32a4a4ed04c9a429d
Zohaifa <141559676+Zohaifa@users.noreply.github.com>
2023-11-27 15:59:21 +0600

    Create 1564.c

---
+++ 1564.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    while((scanf("%d", &n)!=EOF)){
+
+        if(n>0){
+            printf("vai ter duas!\n");
+        }
+        else if(n==0){
+            printf("vai ter copa!\n");
+        }
+    }
+
+    return 0;
+
+}
+
+
+1
