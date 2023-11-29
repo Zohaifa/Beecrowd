53a88f8e046f19f28e4237feabc3f83981f29728
Zohaifa <141559676+Zohaifa@users.noreply.github.com>
2023-11-29 10:52:29 +0600

    Create 1182.c

---
+++ 1182.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    double a[12][12], sum, avg;
+    int i, j, t;
+    char c;
+    scanf("%d %c", &t, &c);
+
+    for(i=0; i<12; i++){
+        for(j=0; j<12; j++){
+            scanf("%lf", &a[i][j]);
+        }
+    }
+    sum = 0;
+    for(i=0; i<12; i++){
+        sum += a[i][t];
+    }
+    if(c == 'S')
+        printf("%.1lf\n", sum);
+    if(c == 'M')
+        printf("%.1lf\n", sum/12);
+
+    return 0;
+}
